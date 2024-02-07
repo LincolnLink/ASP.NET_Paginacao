6af54ea942e211c132e43b3441e142c1f05fdcca
Lincoln <link7-7@hotmail.com>
2024-02-07 00:48:50 -0300

    css

--- Front End/src/main.jsx
+++ Front End/src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import "primereact/resources/themes/lara-light-blue/theme.css";
+
+//import { PrimeReactProvider } from 'primereact/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
